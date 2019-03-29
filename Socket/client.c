76a7f0bbd9f187c558db925c07d528b463d84345
Lucas Santiago <santiagolucas643@gmail.com>
2019-03-29 14:58:00 -0300

    Sokets finalized

---
+++ Socket/client.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define MSGSIZE 20
+#define PORT 4567
+
+int main(int argc, char* argv[]){
+    struct sockaddr_in client_addr, server_addr;
+    int client_fd;
+    char read_n[MSGSIZE], write_n[MSGSIZE];
+    
+    int number = 1;
+
+    if (argc < 2){
+        fprintf(stderr, "Input number not specified.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (client_fd < 0){
+        fprintf(stderr, "\nClient Socket Failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, '0', sizeof(server_addr));
+    server_addr.sin_family = AF_INET; 
+    server_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) < 0){ 
+        printf("\nInvalid address/Address not supported\n"); 
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(client_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) 
+    { 
+        fprintf(stderr, "\nClient Connection Failed\n"); 
+        exit(EXIT_FAILURE);
+    }
+
+    srand(time(NULL));
+    int how_many = atoi(argv[1]);
+
+    for (int i = 0; i < how_many; i++){
+        number += rand() % 100;
+        sprintf(write_n, "%d", number);
+        send(client_fd, write_n, MSGSIZE, 0);
+
+        read(client_fd, read_n, MSGSIZE);
+
+        if (atoi(read_n)) printf("Yes.\n");
+        else printf("No.\n");
+    }
+
+    send(client_fd, "0", MSGSIZE, 0);
+    shutdown(client_fd, 2);
+    printf("\nCLIENT SIGTERM\n");
+
+    exit(0);
+}
--- Socket/server.c
+++ Socket/server.c
@@ -4,18 +4,19 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include "../prime.h"
 
 #define MSGSIZE 20
-#define PORT 8088
+#define PORT 4567
 
 
-void server_socket_create(){
-    int server_fd, client_fd;
+int main(int argc, char* argv[]){
     int opt = 1;
+    int client_fd, server_fd;
+    socklen_t cli_addr;
+    char read_n[MSGSIZE], write_n[MSGSIZE];
     struct sockaddr_in server_addr, client_addr;
 
     // Server socket accepts only IPv4 adresses
@@ -30,52 +31,51 @@ void server_socket_create(){
     // Creation socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0){
-        fprintf(stderr, "Socket Failed.");
+        fprintf(stderr, "\nServer Socket Failed.\n");
         exit(EXIT_FAILURE);
     }
 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        fprintf(stderr, "Operation Failed.");
+        fprintf(stderr, "\nOperation Failed.\n");
         exit(EXIT_FAILURE);
     }
 
     // Binding address to socket
     int server_chain = bind(server_fd, (struct sockaddr*) &server_addr, sizeof(server_addr));
     if (server_chain < 0){
-        fprintf(stderr, "Bind Failed.");
+        fprintf(stderr, "\nBind Failed.\n");
         exit(EXIT_FAILURE);
     }
 
     // Make socket listen
     int server_ears = listen(server_fd, 5);
     if (server_ears < 0){
-        fprintf(stderr, "Listen Failed.");
+        fprintf(stderr, "\nListen Failed.\n");
         exit(EXIT_FAILURE);
     }
 
-    client_fd = accept(server_fd, (struct sockaddr*) &client_addr, sizeof(client_addr));
+    cli_addr = sizeof(client_addr);
+    client_fd = accept(server_fd, (struct sockaddr*) &client_addr, &cli_addr);
     if (client_fd < 0){
-        fprintf(stderr, "Accept Failed.");
+        fprintf(stderr, "\nAccept Failed.\n");
         exit(EXIT_FAILURE);
     }
-}
 
-int server_socket_rw(int client_fd, int server_fd){
-    
-    char read_n[MSGSIZE];
     read(client_fd, read_n, MSGSIZE);
     int out = atoi(read_n);
 
     while (out){
-        printf("Is number %d prime?\n", out);
+        printf("\nIs number %d prime?\n", out);
 
-        if (prime(out)) send(client_fd, "Yes.", MSGSIZE, 0);
-        else send(client_fd, "No.", MSGSIZE, 0);
+        if (prime(out)) send(client_fd, "1", MSGSIZE, 0);
+        else send(client_fd, "0", MSGSIZE, 0);
 
         read(client_fd, read_n, MSGSIZE);
         out = atoi(read_n);
     }
 
     shutdown(server_fd, 2);
-    return 0;
+    printf("\nSERVER SIGTERM\n");
+
+    exit(0);
 }
