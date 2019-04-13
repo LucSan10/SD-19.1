98bff62ca6b5bf6b85491b07d73179abf5c1d36f
Lucas Santiago <santiagolucas643@gmail.com>
2019-04-13 09:39:44 -0300

    Adds random value function

--- Spinlock/spinlock.c
+++ Spinlock/spinlock.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdatomic.h>
-
+#include <time.h>
 
 atomic_flag held = ATOMIC_FLAG_INIT;
 
@@ -13,6 +13,13 @@ void release(volatile atomic_flag* lock){
     atomic_flag_clear(lock);
 }
 
+int generate_random_value(int start, int end){
+    return rand() % (end - start) + start;
+}
+
 int main(){
+    // New seed for rand().
+    srand(time(NULL));
+
     
 }
