19341fecba30f70160e3cdf225810b2fc427568d
Lucas Vieira <lucasvg@poli.ufrj.br>
2019-06-06 20:05:23 -0300

    Member reads each member

--- Assignment_3/src/Member.py
+++ Assignment_3/src/Member.py
@@ -2,6 +2,7 @@ import time
 import socket
 from src.Message import Message
 from src.Message import MessageType
+import json
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 0 # PORT 0 will make the system automatically assign a port to it
@@ -9,6 +10,7 @@ BUFFERSIZE = 1024
 class Member:
     orchestratorAddress = None
     socket = None
+    swarmMembers = []
 
     def __init__(self, orchestratorAddress):
         self.orchestratorAddress = orchestratorAddress
@@ -20,5 +22,10 @@ class Member:
         print(message.toByteStr(), flush=True)
         self.socket.sendto(message.toByteStr(), self.orchestratorAddress)
         response, address = self.socket.recvfrom(1024)
-        print(response, flush=True)
-        
+        print("Swarm members: %s" % (response), flush=True)
+        self.swarmMembers = json.loads(response)
+        self.connectWithOtherMembers()
+    
+    def connectWithOtherMembers(self):
+        for member in self.swarmMembers:
+            print(member)
